b8e5fe63c2e34b987a3eece64eca99324b43c6cd
victoriats1 <vtsolova17@gmail.com>
2025-05-21 19:20:55 +0300

    buton

--- victoria_it/8G-24-25-Koren-kvadraten-ot-4/button.js
+++ victoria_it/8G-24-25-Koren-kvadraten-ot-4/button.js
@@ -1,5 +1,5 @@
 function showSpecial(){
-    const coffees= ["Latte", "Cappucino", "Mocha", "Cold brew"];
+    const coffees= [ "Cappucino", "Espresso", "Cold brew"];
     const pick = coffees[Math.floor(Math.random()*coffees.length)]
     document.getElementById("special").innerText="Todays special is: " + pick;
 }
