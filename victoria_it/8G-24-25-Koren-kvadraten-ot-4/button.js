886e617b3dc10b2128835defa7ec895bd34297cc
Yana <yana.al.dimitrova@gmail.com>
2025-05-23 21:54:19 +0300

    lastchanges

--- victoria_it/8G-24-25-Koren-kvadraten-ot-4/button.js
+++ victoria_it/8G-24-25-Koren-kvadraten-ot-4/button.js
@@ -1,5 +1,5 @@
 function showSpecial(){
     const coffees= [ "Cappucino", "Espresso", "Cold brew"];
     const pick = coffees[Math.floor(Math.random()*coffees.length)]
-    document.getElementById("special").innerText="Todays special is: " + pick;
+    document.getElementById("special").innerText="Today's special is: " + pick;
 }
